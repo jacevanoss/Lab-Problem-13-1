7bbc694311d0780a80bc5e4b378281bf14b9f4d9
Jace Van Oss <j-vanoss@onu.edu>
2024-12-05 08:51:45 -0500

    completed P13B-1

--- Lab 13 Part B.cpp
+++ Lab 13 Part B.cpp
@@ -1,1 +1,40 @@
- 
+#include <iostream>
+
+using namespace std;
+
+bool isLeapYear(int year);
+
+int main() {
+
+	int userInput;
+
+	while (true) {
+		cout << "Enter a year or Q to quit: ";
+		cin >> userInput;
+		if (cin.fail()) {
+			break;
+		}
+		if (isLeapYear(userInput)) {
+			cout << userInput << " is a leap year." << endl;
+		}
+		else {
+			cout << userInput << " is not a leap year." << endl;
+		}
+	}
+
+	return 0;
+}
+bool isLeapYear(int year) {
+	if (year % 4 == 0) {
+		if (year % 100 == 0) {
+			if (year % 400 == 0) {
+				return true;
+			}
+			else {
+				return false;
+			}
+		}
+		return true;
+	}
+	return false;
+}
