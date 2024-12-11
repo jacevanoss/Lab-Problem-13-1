c0bf6c9a4cfad8b67619d10d91820a5ba8cb030c
Jace Van Oss <j-vanoss@onu.edu>
2024-12-11 14:48:12 -0500

    Completed P13B-4

--- Lab 13 Part B.cpp
+++ Lab 13 Part B.cpp
@@ -1,108 +1,151 @@
 #include <iostream>
+#include <iomanip>
 #include <string>
 
 using namespace std;
 
-//bool isLeapYear(int year);
-//int daysInMonth(int month, int year);
+bool isLeapYear(int year);
+int daysInMonth(int month, int year);
 int dayOfWeek(int month, int day, int year);
-string getWeekdayName(int dayOfWeek);
-string getMonthName(int month);
+int getMonthName(const string& monthName);
+void printCalender(int month, int year);
 
 int main() {
 
-	string userInput;
 	int month;
 	int year;
-	int day;
+	string inputMonth;
+	string inputYear;
 
 	while (true) {
-		cout << "Enter a date or Q to quit: ";
-		cin >> userInput;
-		if (userInput == "Q" || userInput == "q") {
+		cout << "Enter a month and year or Q to quit: ";
+		cin >> inputMonth;
+		if (inputMonth == "Q" || inputMonth == "q") {
 			break;
 		}
-		month = stoi(userInput);
-		cin >> day >> year;
-		int Dow = dayOfWeek(month, day, year);
-		string weekday = getWeekdayName(Dow);
-		string monthName = getMonthName(month);
-		cout << weekday << ", " << monthName << " " << day << ", " << year << endl;
-
-
+		else {
+			month = getMonthName(inputMonth);
+			if (month != -1) {
+				cin >> inputYear;
+				year = stoi(inputYear);
+				printCalender(month, year);
+			}
+			else {
+				cout << "Invalid Month" << endl;
+			}
+		}
 	}
 
 	return 0;
 }
-//bool isLeapYear(int year) {
-	//if (year % 4 == 0) {
-		//if (year % 100 == 0) {
-			//if (year % 400 == 0) {
-				//return true;
-			//}
-			//else {
-				//return false;
-			//}
-		//}
-		//return true;
-	//}
-	//return false;
-//}
-//int daysInMonth(int month, int year) {
-	//if (month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12) {
-		//return 31;
-	//}
-	//if (month == 4 || month == 6 || month == 9 || month == 11) {
-		//return 30;
-	//}
-	//if (month == 2) {
-		//if (isLeapYear(year)) {
-			//return 29;
-		//}
-		//else {
-			//return 28;
-		//}
-	//}
-	//return -1;
-//}
+bool isLeapYear(int year) {
+	if (year % 4 == 0) {
+		if (year % 100 == 0) {
+			if (year % 400 == 0) {
+				return true;
+			}
+			else {
+				return false;
+			}
+		}
+		return true;
+	}
+	return false;
+}
+int daysInMonth(int month, int year) {
+	if (month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12) {
+		return 31;
+	}
+	if (month == 4 || month == 6 || month == 9 || month == 11) {
+		return 30;
+	}
+	if (month == 2) {
+		if (isLeapYear(year)) {
+			return 29;
+		}
+		else {
+			return 28;
+		}
+	}
+}
 int dayOfWeek(int month, int day, int year) {
-	if (month == 1 || month == 2) {
+	if (month <= 2){
 		month += 12;
 		year -= 1;
 	}
-	int q = day;
-	int m = month;
-	int y = year % 100;
-	int c = year / 100;
-	int zellers = (q + (13 * (m + 1)) / 5 + y + y / 4 + c / 4 + 5 * c) % 7;
-	return zellers;
+	int k = year % 100;
+	int j = year / 100;
+	int h = (day + (13 * (month + 1)) / 5 + k + k / 4 + j / 4 + 5 * j) % 7;
+	return (h + 5) % 7;
 }
-string getWeekdayName(int dayOfWeek) {
-	switch (dayOfWeek) {
-		case 0: return "Saturday";
-		case 1: return "Sunday";
-		case 2: return "Monday";
-		case 3: return "Tuesday";
-		case 4: return "Wednesday";
-		case 5: return "Thursday";
-		case 6: return "Friday";
-		default: return "Invalid day";
+int getMonthName(const string& monthName) {
+	if (monthName == "January") {
+		return 1;
+	}
+	else if (monthName == "February") {
+		return 2;
+	}
+	else if (monthName == "March") {
+		return 3;
+	}
+	else if (monthName == "April") {
+		return 4;
+	}
+	else if (monthName == "May") {
+		return 5;
+	}
+	else if (monthName == "June") {
+		return 6;
+	}
+	else if (monthName == "July") {
+		return 7;
+	}
+	else if (monthName == "August") {
+		return 8;
+	}
+	else if (monthName == "September") {
+		return 9;
+	}
+	else if (monthName == "October") {
+		return 10;
+	}
+	else if (monthName == "November") {
+		return 11;
+	}
+	else if (monthName == "December") {
+		return 12;
+	}
+	else {
+		return -1;
 	}
 }
-string getMonthName(int month) {
-	switch (month) {
-		case 1: return "January";
-		case 2: return "Febuary";
-		case 3: return "March";
-		case 4: return "April";
-		case 5: return "May";
-		case 6: return "June";
-		case 7: return "July";
-		case 8: return "August";
-		case 9: return "September";
-		case 10: return "October";
-		case 11: return "November";
-		case 12: return "December";
-		default: return "Invalid Month";
+void printCalender(int month, int year) {
+	string calendar[] = {
+		"                   1  2  3  4  5  6  7 ",
+		" 2  3  4  5  6  7  8  9 10 11 12 13 14 ",
+		" 9 10 11 12 13 14 15 16 17 18 19 20 21 ",
+		"16 17 18 19 20 21 22 23 24 25 26 27 28 ",
+		"23 24 25 26 27 28 29 30 31             ",
+		"30 31                                  "
+	};
+	
+	cout << "Su Mo Tu We Th Fr Sa" << endl;
+
+	int startDay = dayOfWeek(month, 1, year);
+	int numDays = daysInMonth(month, year);
+
+	int day = 1;
+	for (int i = 0; i < startDay + 1; i++) {
+		cout << "   ";
 	}
+	for (int i = startDay + 1; day <= numDays; i++) {
+		cout << setw(2) << day << " ";
+		day++;
+		if (i == 6) {
+			cout << endl;
+			i = -1;
+		}
+	}
+	cout << endl;
 }
+	
